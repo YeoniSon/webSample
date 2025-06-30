586a1354cb594e2e9f6908e530b5d42ca1019c05
YeoniSon <sjy8211@gmail.com>
2025-06-30 14:06:38 +0900

    스프링 mvc_http요청응답 파라미터를 이용한 request, 파라미터의 크기가 클 경우 이용하는 RequestBody에 대한 실습 내용

--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -1,5 +1,6 @@
 package com.example.websample.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,15 +11,50 @@ public class SampleController {
 
     //외부 url을 찾아올수 있도록 하기 위해
 //    @RequestMapping(value = "/order/1", method = RequestMethod.GET)
-    @GetMapping("/order/1")
-    public  String getOrder() {
-        log.info("Get some order");
-        return "OrderId:1, orderAmount:1000";
+    @GetMapping("/order/{orderId}")
+    public  String getOrder(@PathVariable("orderId") String id) {
+        log.info("Get some order : " + id);
+        return "OrderId:" + id +", orderAmount:1000";
     }
 
+    @DeleteMapping("/order/{orderId}")
+    public  String deleteOrder(@PathVariable("orderId") String id) {
+        log.info("Delete some order : " + id);
+        return "Delete orderId:" + id;
+    }
+
+    // 파라미터로 이용해서 가져올수 있음 -> 게시판 같은 경우에 많이 사용(페이지, 서치키워드)
+    @GetMapping("/order")
+    public  String getOrderWithParam(
+            @RequestParam(value = "orderId", required=false, defaultValue = "defaultId") String id,
+            @RequestParam("orderAmount") Integer amount) {
+        log.info("Get some order : " + id + ", amount : " + amount);
+        return "OrderId:" + id +", orderAmount:" + amount;
+    }
+
+    //RequsetBody 사용 -> 큰 데이터 사용시 사용한다.
     @PostMapping("/order")
+    public  String createOrder(
+            @RequestBody CreateOrderRequest createOrderRequest,
+            @RequestHeader String userAccountId) {
+        log.info("Get some order : " + createOrderRequest +
+                ", userAccountId : " + userAccountId);
+        return "OrderId:" + createOrderRequest.getOrderId() +", orderAmount:"
+                + createOrderRequest.getOrderAmount();
+    }
+
+    @PutMapping("/order")
     public String createOrder() {
         log.info("Create order");
         return "order created -> orderId:1, orderAmount:1000";
     }
+
+
+    @Data //lombok을 이용한것
+    public static class CreateOrderRequest{
+        private String orderId;
+        private Integer orderAmount;
+
+    }
+
 }
