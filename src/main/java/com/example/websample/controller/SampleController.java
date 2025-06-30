f00f2669498db9c23134746b459a695fb8b91e75
YeoniSon <sjy8211@gmail.com>
2025-06-30 23:54:46 +0900

    스프링 mvc_필터 예외처리 - 3 예외처리 GlobalExceptionHandler로 이동시키는 실습

--- src/main/java/com/example/websample/controller/SampleController.java
+++ src/main/java/com/example/websample/controller/SampleController.java
@@ -42,53 +42,6 @@ public class SampleController {
         return "OrderId:" + id + ", orderAmount:1000";
     }
 
-//    @ResponseStatus(HttpStatus.FORBIDDEN)
-//    @ExceptionHandler(IllegalAccessException.class)
-//    public ErrorResponse handleException(IllegalAccessException e) {
-//        log.error("IllegalAccessException is occurred.",e);
-//
-////        return "INVALID_ACCESS";
-//        return new ErrorResponse("INVALID_ACCESS",
-//                "IllegalAccessException is occurred.");
-//    }
-
-    //ResponseEntity 활용한 예시
-    @ExceptionHandler(IllegalAccessException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalAccessException(
-            IllegalAccessException e) {
-        log.error("IllegalAccessException is occurred.", e);
-
-//        return "INVALID_ACCESS";
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(ErrorCode.TOO_BIG_ID_ERROR,
-                        "IllegalAccessException is occurred."));
-    }
-
-    @ExceptionHandler(WebSampleException.class)
-    public ResponseEntity<ErrorResponse> handleWebSampleException(
-            WebSampleException e) {
-        log.error("WebSampleException is occurred.", e);
-
-//        return "INVALID_ACCESS";
-        return ResponseEntity
-                .status(HttpStatus.INSUFFICIENT_STORAGE)
-                .body(new ErrorResponse(e.getErrorCode(),
-                        "WebSampleException is occurred."));
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(
-            Exception e) {
-        log.error("WebSampleException is occurred.", e);
-
-//        return "INVALID_ACCESS";
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR,
-                        "Exception is occurred."));
-    }
-
-
 
 
     @DeleteMapping("/order/{orderId}")
